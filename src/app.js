98dbf245e33dbde76e6791a39fb27cc118e79f87
Rafa-progz <rafa.trabajo@outlook.es>
2025-06-29 14:55:37 +0000

    haciendo carta

--- src/app.js
+++ src/app.js
@@ -7,5 +7,13 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  
+  const suits = ["♦" , "♥" , "♠" , "♣"];
+  const numberCard = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1]
+
+  const randomSuits = suits[Math.floor(Math.random() * suits.length)];
+  const randomNumberCard = numberCard[Math.floor(Math.random() * numberCard.length)];
+
+
   console.log("Hello Rigo from the console!");
 };
