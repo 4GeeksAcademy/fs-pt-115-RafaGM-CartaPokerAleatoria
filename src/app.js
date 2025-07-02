30d334c6dada408b1e9629d3c101572672ef583b
Rafa-progz <rafa.trabajo@outlook.es>
2025-07-02 11:27:26 +0000

    echo

--- src/app.js
+++ src/app.js
@@ -5,15 +5,42 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  
-  const suits = ["♦" , "♥" , "♠" , "♣"];
-  const numberCard = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1]
-
-  const randomSuits = suits[Math.floor(Math.random() * suits.length)];
-  const randomNumberCard = numberCard[Math.floor(Math.random() * numberCard.length)];
 
 
-  console.log("Hello Rigo from the console!");
+
+  const getRandom = (array) => {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+  const numeroCarta = document.querySelectorAll(`.value`)
+  const suitsCarta = document.querySelectorAll(`.suit`)
+
+
+
+
+  const mostrarCarta = () => {
+    const suits = ["♦", "♥", "♠", "♣"];
+    const numberCard = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+    const randomSuit = getRandom(suits)
+    const randomNumber = getRandom(numberCard)
+
+
+    suitsCarta.forEach(suit => {
+      suit.textContent = randomSuit;
+      suit.style.color = (randomSuit === "♦" || randomSuit === "♥") ? "red" : "black";
+    });
+    numeroCarta.forEach(numero => {
+      numero.textContent = randomNumber;
+      //numero.style.color = (randomSuit === "♦" || randomSuit === "♥") ? "red" : "black";
+    });
+
+  }
+  window.addEventListener("load", mostrarCarta());
+
+  document.querySelector(`#boton`).addEventListener(`click`, () => {
+    mostrarCarta()
+  })
+  setInterval(mostrarCarta, 3000);
 };
